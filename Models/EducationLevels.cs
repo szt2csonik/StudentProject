e64cb1a2cdee961804e163326d276acacf1d2454
Your Name <szt2csonik@vasvari.org>
2023-09-11 13:01:00 +0200

    educationlevels

---
+++ Models/EducationLevels.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace StudentProjekt.Models
+{
+    public class EducationLevels
+    {
+        public List<string> AllEducationLevel { get; } = new List<string> { "érettségi", "szakmai érettségi", "szakmai vizsga" };
+    }
+}
