8caee5b149c3e664d6643150e577c1449a629231
Your Name <szt2csonik@vasvari.org>
2023-09-11 11:34:13 +0200

    student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,12 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace StudentProject.Models
+public enum SchoolClassType { CLASS_A, CLASS_B, CLASS_C }
+
+namespace StudentProjekt.Models
 {
-    internal class Student
+    public class Student
     {
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            BirthsDay = new DateTime();
+            SchoolYear = 9;
+            SchoolClass = SchoolClassType.CLASS_A;
+            EducationLevel = string.Empty;
+        }
+
+        public Student(string firstName, string lastName, DateTime birthsDay, int year, SchoolClassType schoolClass)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthsDay = birthsDay;
+            SchoolYear = year;
+            SchoolClass = schoolClass;
+            EducationLevel = string.Empty;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthsDay { get; set; }
+        public int SchoolYear { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public string EducationLevel { get; set; }
     }
 }
