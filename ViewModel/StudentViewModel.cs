0aec3e8d41d5f89a0545c0c675dff55ff5cdba16
Your Name <szt2csonik@vasvari.org>
2023-09-11 13:05:35 +0200

    properties

--- ViewModel/StudentViewModel.cs
+++ ViewModel/StudentViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using StudentProjekt.Models;
+using System.Collections.Generic;
 
 namespace StudentProjekt.ViewModel
 {
     public partial class StudentViewModel : ObservableObject
     {
+        private List<string> _educationLevel = new EducationLevels().AllEducationLevel;
         [ObservableProperty]
         private Student student;
+
+        [ObservableProperty]
+        private string selectedEducationLevel;
+
+        [ObservableProperty]
+        private List<string> educationLevels;
 
 
         public StudentViewModel()
         {
             student = new Student();
+            SelectedEducationLevel = string.Empty;
+            EducationLevels = _educationLevel;
         }
     }
 }
